59cc086e2c3f3a5363914502ac21e75b9dfe0010
Marcelo Prates <mpratesjunior@gmail.com>
2022-08-28 11:49:31 -0300

    chore(#4): change commitlint config

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -6,12 +6,12 @@ export const DatabaseProvider = [
     provide: 'SEQUELIZE',
     useFactory: async () => {
       const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'password',
-        database: 'nest',
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         operatorsAliases: {
           [Op.like]: '$like',
           [Op.and]: '$and',
