d192d40fdfcc47af3f9aca21413374dd70accae9
Marcelo Prates <mpratesjunior@gmail.com>
2022-12-15 15:49:57 -0300

    feat(#20): add validation on types

--- src/types/query-params.dto.ts
+++ src/types/query-params.dto.ts
@@ -1,6 +1,17 @@
+import { IsNumberString, IsOptional } from 'class-validator';
+import { IsUUID } from 'sequelize-typescript';
+
 export class QueryParams {
+  @IsOptional()
+  @IsUUID(4)
   id?: string;
+  @IsOptional()
+  @IsNumberString()
   limit?: string;
+  @IsOptional()
+  @IsNumberString()
   page?: string;
+  @IsOptional()
+  @IsNumberString()
   offset?: string;
 }
