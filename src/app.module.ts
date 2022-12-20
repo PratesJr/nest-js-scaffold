e744007e1b468808c5df51dca71b5874299c6b4d
Marcelo Prates <mpratesjunior@gmail.com>
2022-12-20 17:05:02 -0300

    wip(#22): caching on redis DBy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import {
   CacheModule,
   MiddlewareConsumer,
@@ -10,13 +11,32 @@ import { DatabaseModule } from './database/database.module';
 import { HttpLogMiddleware } from './middleware/http-log.middleware';
 import { AuthModule } from './module/auth/auth.module';
 import { UserModule } from './module/user/user.module';
+// import * as redisStore from 'cache-manager-redis-store';
+const cacheManager = require('cache-manager');
+const redisStore = require('cache-manager-redis-store');
+
+const redisCache = cacheManager.caching({
+  store: redisStore,
+  host: 'localhost', // default value
+  port: 6379, // default value
+  // eslint-disable-next-line camelcase
+  auth_pass: 'redisPWD',
+  db: 0,
+  ttl: 600,
+});
 
 @Module({
   imports: [
     DatabaseModule,
     AuthModule,
     UserModule,
-    CacheModule.register({ isGlobal: true }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisCache,
+      host: 'localhost',
+      port: 6379,
+      ttl: 500,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
